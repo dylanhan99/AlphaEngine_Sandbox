ac8231e2a6f2e61363d5037606b69a04061a4fd8
dylanhan99 <dylanhan99@gmail.com>
2022-12-27 13:50:04 +0800

    Demo

--- MyDemo/main.cpp
+++ MyDemo/main.cpp
@@ -21,6 +21,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	int gGameRunning = 1;
 
+	float obj1X = 0.0f, obj1Y = 0.0f; // Position variables for object 1
+
+	AEGfxVertexList* pMesh1 = 0;
+	AEGfxVertexList* pMesh2 = 0; // Pointer to Mesh (Model)
+
+	AEGfxTexture* pTex1; // Pointer to Texture (Image)
+
+
 	// Variable declaration end
 	///////////////////////////
 
@@ -31,9 +39,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// Using custom window procedure
 	AESysInit(hInstance, nCmdShow, 800, 600, 1, 60, true, NULL);
 
-
 	// Changing the window title
 	AESysSetWindowTitle("My New Demo!");
+
+	//set background color
+	AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
 
 	// reset the system modules
 	AESysReset();
@@ -46,6 +56,40 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	////////////////////////////////
 	// Creating the objects (Shapes)
 
+	// Informing the library that we're about to start adding triangles
+	AEGfxMeshStart();
+
+	// 1 triangle at a time
+	// X, Y, Color, texU, texV
+	AEGfxTriAdd(
+		-25.5f, -25.5f, 0xFFFF0000, 0.0f, 0.0f,
+		25.5f, 0.0f, 0xFFFF0000, 0.0f, 0.0f,
+		-25.5f, 25.5f, 0xFFFF0000, 0.0f, 0.0f);
+
+	// Saving the mesh (list of triangles) in pMesh1
+
+	pMesh1 = AEGfxMeshEnd();
+	AE_ASSERT_MESG(pMesh1, "Failed to create mesh 1!!");
+
+	// Informing the library that we're about to start adding triangles
+	AEGfxMeshStart();
+
+	// This shape has 2 triangles
+	AEGfxTriAdd(
+		-30.0f, -30.0f, 0x00FF00FF, 0.0f, 1.0f,
+		30.0f, -30.0f, 0x00FFFF00, 1.0f, 1.0f,
+		-30.0f, 30.0f, 0x0000FFFF, 0.0f, 0.0f);
+
+	AEGfxTriAdd(
+		30.0f, -30.0f, 0x00FFFFFF, 1.0f, 1.0f,
+		30.0f, 30.0f, 0x00FFFFFF, 1.0f, 0.0f,
+		-30.0f, 30.0f, 0x00FFFFFF, 0.0f, 0.0f);
+
+	// Saving the mesh (list of triangles) in pMesh2
+
+	pMesh2 = AEGfxMeshEnd();
+	AE_ASSERT_MESG(pMesh2, "Failed to create mesh 2!!");
+
 	// Creating the objects (Shapes) end
 	////////////////////////////////////
 
@@ -53,6 +97,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	////////////////////////////
 	// Loading textures (images)
+
+	// Texture 1: From file
+	pTex1 = AEGfxTextureLoad("Assets/PlanetTexture.png");
+	AE_ASSERT_MESG(pTex1, "Failed to create texture1!!");
 
 	// Loading textures (images) end
 	//////////////////////////////////
@@ -76,12 +124,57 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		///////////////////
 		// Game loop update
 
+		// Blending mode
+		if (AEInputCheckCurr(AEVK_1))
+			AEGfxSetBlendMode(AE_GFX_BM_NONE);
+		else if (AEInputCheckCurr(AEVK_2))
+			AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+		else if (AEInputCheckCurr(AEVK_3))
+			AEGfxSetBlendMode(AE_GFX_BM_ADD);
+
 		// Game loop update end
 		///////////////////////
 
 
 		//////////////////
 		// Game loop draw
+
+		// No tint
+		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// Drawing object 1
+		AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+		// Set position for object 1
+		AEGfxSetPosition(0.0f, 0.0f);
+		// No texture for object 1
+		AEGfxTextureSet(pTex1, 0, 0);
+		// Drawing the mesh (list of triangles)
+		AEGfxMeshDraw(pMesh1, AE_GFX_MDM_TRIANGLES);
+
+		// Drawing object 2 - (first) - No tint
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		// Set position for object 2
+		AEGfxSetPosition(100.0f, -60.0f);
+		// No tint
+		AEGfxSetTintColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// Set texture
+		AEGfxTextureSet(pTex1, 0.0f, 0.0f);
+		// Set Transparency
+		AEGfxSetTransparency(1.0f);
+		// Drawing the mesh (list of triangles)
+		AEGfxMeshDraw(pMesh2, AE_GFX_MDM_TRIANGLES);
+
+		// Drawing object 3 - (Second) - Blue tint
+		AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+		// Set position for object 3
+		AEGfxSetPosition(100.0f, 60.0f);
+		// Add Blue tint
+		AEGfxSetTintColor(0.0f, 0.0f, 1.0f, 1.0f);
+		// Set texture
+		AEGfxTextureSet(pTex1, 0.0f, 0.0f);
+		// Set Transparency
+		AEGfxSetTransparency(1.0f);
+		// Drawing the mesh (list of triangles)
+		AEGfxMeshDraw(pMesh2, AE_GFX_MDM_TRIANGLES);
 
 		// Game loop draw end
 		/////////////////////
@@ -95,6 +188,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 			gGameRunning = 0;
 	}
 
+	// Freeing the objects and textures
+	AEGfxMeshFree(pMesh1);
+	AEGfxMeshFree(pMesh2);
+	AEGfxTextureUnload(pTex1);
 
 	// free the system
 	AESysExit();
